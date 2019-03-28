2bfcc23b8708595d3cfc6d2b3efcf622ae382b79
aberl <aberl-workload@163.com>
2019-03-28 22:22:38 +0800

    database sql script

---
+++ chengxuxiaoba.sql
@@ -0,0 +1,144 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 80015
+Source Host           : localhost:3306
+Source Database       : chengxuxiaoba
+
+Target Server Type    : MYSQL
+Target Server Version : 80015
+File Encoding         : 65001
+
+Date: 2019-03-28 22:19:19
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for account
+-- ----------------------------
+DROP TABLE IF EXISTS `account`;
+CREATE TABLE `account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `headerImg` varchar(255) DEFAULT NULL,
+  `mobilePhoneNo` varchar(20) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `wechatAccount` varchar(255) DEFAULT NULL,
+  `wechatHeaderImg` varchar(255) DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT '1',
+  `createDateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updateDateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for answer
+-- ----------------------------
+DROP TABLE IF EXISTS `answer`;
+CREATE TABLE `answer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `issueId` int(11) NOT NULL,
+  `content` varchar(255) NOT NULL,
+  `answererId` int(11) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '1',
+  `createDateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for course
+-- ----------------------------
+DROP TABLE IF EXISTS `course`;
+CREATE TABLE `course` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `images` varchar(1000) DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT '1',
+  `createDateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for coursemodule
+-- ----------------------------
+DROP TABLE IF EXISTS `coursemodule`;
+CREATE TABLE `coursemodule` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `courseId` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  `images` varchar(1000) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for evaluate
+-- ----------------------------
+DROP TABLE IF EXISTS `evaluate`;
+CREATE TABLE `evaluate` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `videoId` int(11) NOT NULL,
+  `content` varchar(500) NOT NULL,
+  `starts` int(11) NOT NULL,
+  `accountId` int(20) NOT NULL,
+  `praiseCount` int(11) NOT NULL DEFAULT '0',
+  `status` int(11) NOT NULL DEFAULT '1',
+  `createDateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for issue
+-- ----------------------------
+DROP TABLE IF EXISTS `issue`;
+CREATE TABLE `issue` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `videoId` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `content` varchar(1000) NOT NULL,
+  `questionerId` int(11) NOT NULL,
+  `answerCount` int(11) NOT NULL DEFAULT '0',
+  `status` int(11) NOT NULL DEFAULT '1',
+  `createDateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for validationcode
+-- ----------------------------
+DROP TABLE IF EXISTS `validationcode`;
+CREATE TABLE `validationcode` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `mobilePhoneNo` varchar(20) NOT NULL,
+  `validationCode` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `expireTime` datetime DEFAULT NULL,
+  `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `isActive` bit(1) NOT NULL DEFAULT b'1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for video
+-- ----------------------------
+DROP TABLE IF EXISTS `video`;
+CREATE TABLE `video` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `courseModuleId` int(11) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `duration` int(11) NOT NULL COMMENT '视频时长，分钟为单位',
+  `description` varchar(4000) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `images` varchar(255) DEFAULT NULL,
+  `viewCount` int(11) NOT NULL DEFAULT '0',
+  `praiseCount` int(11) NOT NULL DEFAULT '0',
+  `status` int(11) NOT NULL DEFAULT '1',
+  `createDateTime` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_index` (`courseModuleId`,`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
